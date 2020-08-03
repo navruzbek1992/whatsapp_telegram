39b449af01341b46799dec85885a45d64f8f3669
navruzbek karamatov <40276486+navruzbek1992@users.noreply.github.com>
2020-08-03 20:56:08 +0900

    refactored scripts

--- wa_tg.py
+++ wa_tg.py
@@ -1,98 +1,90 @@
 import pyautogui as pyg
 import time
+import pandas as pd
 from datetime import datetime
 from telethon.sync import TelegramClient
-from telethon.tl.types import InputPeerEmpty
-from telethon.tl.types import PeerChannel
-from telethon.tl.types import ChannelParticipantsSearch
-from telethon.tl.functions.channels import GetParticipantsRequest
-from telethon.tl.functions.messages import GetDialogsRequest
-from telethon.tl.functions.messages import GetHistoryRequest
-from telethon.errors.rpcerrorlist import SessionPasswordNeededError
-from telethon.sync import TelegramClient
-from telethon import functions, types
-from telethon.tl.types import InputPeerEmpty
-from telethon.tl.types import PeerChannel
 import asyncio
 import configparser
 import json
+import re
 import shutil
 from utils import *
+import os
+import nest_asyncio
+import subprocess
 
-doc = input("How many docs?")
+nest_asyncio.apply()
+
+doc = input("How many docs?: ")
+
+
+async def send_data(new_path):
+    for file_name in os.listdir(new_path):
+        if "python_gen_" in file_name:
+            file = new_path + "/" + file_name
+            await client.send_file("me", file)
+
+
+def send_to_tg(new_path):
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(send_data(new_path))
+    loop.close()
+
 
 if __name__ == "__main__":
+
     print("Please dont touch your mouse until python downloads data from whatsapp")
-    win_search = pyg.locateCenterOnScreen(
-        "C:/Users/.../Desktop/python_automate/win_search.png"
+    username = input("What is username for this PC?: ")
+
+    subprocess.Popen(
+        [
+            "C:/Users"
+            + "/"
+            + str(username)
+            + "/"
+            + "AppData/Local/WhatsApp/WhatsApp.exe",
+            "-new-tab",
+        ]
     )
-    x = win_search[0]
-    y = win_search[1]
-    pyg.click(x, y)
-    pyg.typewrite("whatsapp")
-    pyg.press("enter")
-
-    time.sleep(10)  # 15 seconds sleep
-
-    wait = True
-    while wait:
-        win_search = pyg.locateCenterOnScreen(
-            "C:/Users/.../Desktop/python_automate/whatsapp_search.png"
-        )
-        if type(win_search) == type(None):
-            wait = True
-        else:
-            wait = False
-
-    win_search = pyg.locateCenterOnScreen(
-        "C:/Users/.../Desktop/python_automate/whatsapp_search.png"
-    )
+    time.sleep(10)
+    waiter("whatsapp_search.png")
+    win_search = pyg.locateCenterOnScreen("whatsapp_search.png")
     x = win_search[0]
     y = win_search[1]
     pyg.click(x, y)
     pyg.typewrite("test")
     pyg.press("enter")
 
-    time.sleep(5)  # 5 seconds sleep
+    time.sleep(5)
 
-    win_search = pyg.locateCenterOnScreen(
-        "C:/Users/.../Desktop/python_automate/whatsapp_attach3dots.png"
-    )
+    waiter("whatsapp_attach3dots.png")
+    win_search = pyg.locateCenterOnScreen("whatsapp_attach3dots.png")
     x = win_search[0] + 15
     y = win_search[1]
     pyg.click(x, y)
 
-    time.sleep(1)  # 5 seconds sleep
-
-    win_search = pyg.locateCenterOnScreen(
-        "C:/Users/.../Desktop/python_automate/whatsapp_groupinfo.png"
-    )
+    time.sleep(2)
+    win_search = pyg.locateCenterOnScreen("whatsapp_groupinfo.png")
     x = win_search[0]
     y = win_search[1]
     pyg.click(x, y)
 
-    time.sleep(1)  # 5 seconds sleep
-
-    win_search = pyg.locateCenterOnScreen(
-        "C:/Users/.../Desktop/python_automate/whatsapp_medialinks.png"
-    )
+    waiter("whatsapp_medialinks.png")
+    win_search = pyg.locateCenterOnScreen("whatsapp_medialinks.png")
     x = win_search[0]
     y = win_search[1]
     pyg.click(x, y)
 
-    time.sleep(1)  # 5 seconds sleep
-
+    time.sleep(2)  # 5 seconds sleep
     doc_number = range(1, int(doc) + 1)
 
-    win_search = pyg.locateCenterOnScreen(
-        "C:/Users/.../Desktop/python_automate/whatsapp_docs.png"
-    )
+    win_search = pyg.locateCenterOnScreen("whatsapp_docs.png")
     x_doc = win_search[0]
     y_doc = win_search[1]
 
     for doc in doc_number:
         print(doc)
-        y_axis = 200 * doc
+        y_axis = 150 * doc
 
         pyg.click(x_doc, y_doc)
         time.sleep(3)  # 5 seconds sleep
@@ -100,27 +92,16 @@ if __name__ == "__main__":
         pyg.moveRel(-30, y_axis)
         pyg.click()
 
-        time.sleep(1)  # 5 seconds sleep
-
-        wait = True
-        while wait:
-            win_search = pyg.locateCenterOnScreen(
-                "C:/Users/.../Desktop/python_automate/win_save.png"
-            )
-            if type(win_search) == type(None):
-                wait = True
-            else:
-                wait = False
+        time.sleep(2)
 
         unix_time = time.time()
         pyg.press("left")
         pyg.typewrite("python_gen" + "_" + str(unix_time).split(".")[0])
 
-        time.sleep(1)  # 5 seconds sleep
+        time.sleep(2)  # 5 seconds sleep
 
-        win_search = pyg.locateCenterOnScreen(
-            "C:/Users/.../Desktop/python_automate/win_save.png"
-        )
+        waiter("win_save.png")
+        win_search = pyg.locateCenterOnScreen("win_save.png")
         x = win_search[0]
         y = win_search[1]
         pyg.click(x, y)
@@ -133,7 +114,7 @@ if __name__ == "__main__":
     os.mkdir(file)
     new_path = os.path.abspath(file)
 
-    downloaded_path = r"C:\Users\...\Downloads"
+    downloaded_path = "C:/Users" + "/" + str(username) + "/" + "Downloads"
 
     for file_name in os.listdir(downloaded_path):
         if "python_gen_" in file_name:
@@ -144,12 +125,7 @@ if __name__ == "__main__":
 
     client = TelegramClient(info[0], info[2], info[3])
 
-    for file_name in os.listdir(new_path):
+    client.start()
+    send_to_tg(new_path)
 
-        print(file_name)
-
-        async def main():
-            await client.send_file("Test", file)
-
-        async with client:
-            client.loop.run_until_complete(main())
+    print("Done!")
